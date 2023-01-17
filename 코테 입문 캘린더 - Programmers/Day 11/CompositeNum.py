bb0a9fd58a456cfc540fa486c46feec8286c710f
jhsseonn <hs125bt@gmail.com>
2023-01-17 21:57:06 +0900

    230117 programmers

--- 코테 입문 캘린더 - Programmers/Day 11/CompositeNum.py
+++ 코테 입문 캘린더 - Programmers/Day 11/CompositeNum.py
@@ -1,1 +1,8 @@
-#1 s
+def solution(n):
+    answer = 0
+    for i in range(2, n+1):
+        for j in range(2, i-1):
+            if (i%j==0):
+                answer+=1
+                break
+    return answer
