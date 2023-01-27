f351e4bcc418e011c6d02fff65e6f7e26246cdd8
jhsseonn <hs125bt@gmail.com>
2023-01-28 00:13:56 +0900

    230127 programmers

--- 코테 입문 캘린더 - Programmers/Day 16/Array.py
+++ 코테 입문 캘린더 - Programmers/Day 16/Array.py
@@ -1,5 +1,12 @@
 def solution(my_string):
-    num1, calc, num2 = map(str, my_string.split(" "))
-    if calc=="+":
-        return int(num1)+int(num2)
-    return int(num1)-int(num2)
+    answer = 0
+    num1, calc, num2 = map(str, my_string.split(' '))
+    if calc=='+':
+        answer = int(num1)+int(num2)
+    else:
+        answer = int(num1)-int(num2)
+    return answer
+
+
+def solution(my_string):
+    return eval(my_string)
