61252ce073b21601adb9b9ac4140591d1b11041f
jhsseonn <hs125bt@gmail.com>
2023-02-06 23:47:32 +0900

    230206 programmers

--- 코테 입문 캘린더 - Programmers/Day 20/Character.py
+++ 코테 입문 캘린더 - Programmers/Day 20/Character.py
@@ -1,26 +1,15 @@
 def solution(keyinput, board):
     answer = []
-    x=0
-    y=0
+    answer.append(0)
+    answer.append(0)
     for key in keyinput:
-        if key=="up":
-            y+=1
-        if key=="down":
-            y-=1
-        if key=="left":
-            x-=1
-        if key=="right":
-            x+=1
-    if abs(x)>=(board[0]//2):
-        if x<0:
-            x = -(board[0]//2)
-        else:
-            x = board[0]//2
-    if abs(y)>=(board[1]//2):
-        if y<0:
-            y = -(board[1]//2)
-        else:
-            y = board[1]//2
-    answer.append(x)
-    answer.append(y)
+        if key=="right" and answer[0]<=(board[0]//2):
+            answer[0]+=1
+        elif key=="left" and abs(answer[0])<=(board[0]//2):
+            answer[0]-=1
+        elif key=="up" and answer[1]<=(board[1]//2):
+            answer[1]+=1
+        elif key=="down" and abs(answer[1])<=(board[1]//2):
+            answer[1]-=1
+    
     return answer
