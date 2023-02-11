bca42d8a665b9a96f982f72999b142b2a004f619
jhsseonn <hs125bt@gmail.com>
2023-02-11 23:44:20 +0900

    230211 programmers

---
+++ 코테 입문 캘린더 - Programmers/Day 20/Add.py
@@ -0,0 +1,21 @@
+def solution(polynomial):
+    answer = ''
+
+    poly = list(map(str, polynomial.split(" ")))
+    num = 0
+    num2 = 0
+    for p in poly:
+        if p!="+":
+            if "x" in p and p!="x":
+                num+=int(p[0])
+            if "x" in p and p=="x":
+                num+=1
+            if "x" not in p:
+                num2+=int(p)
+
+    if num2!=0:
+        answer = "{}x + {}".format(num, num2)
+    else:
+        answer = "{}x".format(num)
+
+    return answer
