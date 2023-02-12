eac94e6fd182c6586e11c022a7a56ac2157d3ae3
jhsseonn <hs125bt@gmail.com>
2023-02-12 16:14:21 +0900

    230212 programmers

--- 코테 입문 캘린더 - Programmers/Day 20/Add.py
+++ 코테 입문 캘린더 - Programmers/Day 20/Add.py
@@ -1,17 +1,16 @@
 def solution(polynomial):
     answer = ''
 
-    poly = list(map(str, polynomial.split(" ")))
+    poly = list(map(str, polynomial.split(" + ")))
     num = 0
     num2 = 0
     for p in poly:
-        if p!="+":
-            if "x" in p and p!="x":
-                num+=int(p[0])
-            if "x" in p and p=="x":
-                num+=1
-            if "x" not in p:
-                num2+=int(p)
+        if ("x" in p) and p!="x":
+            num+=int(p[0])
+        if p=="x":
+            num+=1
+        if "x" not in p:
+            num2+=int(p)
 
     if num2!=0:
         answer = "{}x + {}".format(num, num2)
