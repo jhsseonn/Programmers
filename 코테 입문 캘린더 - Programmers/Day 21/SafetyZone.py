63c755621eeb15b642ba86a45ba39ec12d03f7b7
jhsseonn <hs125bt@gmail.com>
2023-02-17 23:41:50 +0900

    230217 programmers

---
+++ 코테 입문 캘린더 - Programmers/Day 21/SafetyZone.py
@@ -0,0 +1,20 @@
+def solution(board):
+    answer = 0
+    for i in range(0, len(board)):
+        for j in range(0, len(board)):
+            if i==0 or i==len(board)-1:
+                if j!=0 and j!=len(board)-1:
+                    answer+=1
+            elif j==0 or j==len(board)-1:
+                answer+=1
+            elif board[i][j]==1:
+                for x in range(i-1, i+2):
+                    for y in range(j-1, j+2):
+                        if board[x][y]==0:
+                            answer+=1
+    
+    
+
+                    
+
+    return answer
