8f27367e6538096292a54122223636774a740fc5
jhsseonn <hs125bt@gmail.com>
2023-03-06 23:00:28 +0900

    230306 programmers

--- 코테 입문 캘린더 - Programmers/Day 21/Alien.py
+++ 코테 입문 캘린더 - Programmers/Day 21/Alien.py
@@ -1,3 +1,12 @@
 def solution(spell, dic):
-    answer = 0
+    answer = 2
+
+    for word in dic:
+        same = 0
+        for s in spell:
+            if s in word:
+                same+=1
+        if same==len(spell):
+            answer=1
+
     return answer
