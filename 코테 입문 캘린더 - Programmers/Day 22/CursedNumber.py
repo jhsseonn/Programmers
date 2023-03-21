8ee2dd500c07612b265b79c3baf1cc368c1594f0
jhsseonn <hs125bt@gmail.com>
2023-03-21 23:42:43 +0900

    230321 programmers

--- 코테 입문 캘린더 - Programmers/Day 22/CursedNumber.py
+++ 코테 입문 캘린더 - Programmers/Day 22/CursedNumber.py
@@ -1,3 +1,9 @@
-def solution():
-    answer=0
-    return answer
+def solution(n):
+    threeXlist = []
+    for i in range(300):
+        if i%3==0 or ("3" in str(i)):
+            continue
+        else:
+            threeXlist.append(i)
+
+    return threeXlist[n-1]
