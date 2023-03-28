9be0002deeb5eea1853164c777025ce4824f4b64
jhsseonn <hs125bt@gmail.com>
2023-03-28 11:06:10 +0900

    230328 programmers

--- 코테 입문 캘린더 - Programmers/Day 22/FiniteDecimal.py
+++ 코테 입문 캘린더 - Programmers/Day 22/FiniteDecimal.py
@@ -1,39 +1,46 @@
 import math
 
 def solution(a, b):
-    answer = 0
-    a = a // math.gcd(a,b)
-    b = b // math.gcd(a,b)
+    gcd = math.gcd(a,b)
+    a = a//gcd
+    b = b//gcd
 
-    if b == 1:
+    if b == 1 or b == 2 or b == 5:
         return 1
     else:
         b_primes = []
-        for i in range(1, b//2+1):
+        for i in range(2, b//2+1):
             if (b%i==0):
                 b_primes.append(i)
 
-        print(b_primes)
+        if not b_primes:
+            b_primes.append(b)
 
         prime_nums=set()
-        if 2 in b_primes or 5 in b_primes:
-            for prime in b_primes:
-                while(prime%2==0):
-                    prime_nums.add(prime//2)
-                    prime=prime//2
-                while(prime%5==0):
-                    prime_nums.add(prime//5)
-                    prime=prime//5
-        else:
-            prime_nums = b_primes
-
-        print(prime_nums)
+        for prime in b_primes:
+            while(prime%2==0):
+                prime_nums.add(2)
+                prime=prime//2
+            while(prime%5==0):
+                prime_nums.add(5)
+                prime=prime//5
+            if prime!=1: prime_nums.add(prime)
 
         for num in prime_nums:
-            if num!=1 and num!=2 and num!=5:
+            if num!=2 and num!=5:
                 return 2
         
         return 1
-    
 
-print(solution(12, 21))
+
+#다른 사람들 풀이...
+from math import gcd
+def solution(a, b):
+    b //= gcd(a, b)
+    while b%2==0:
+        b//=2
+    while b%5==0:
+        b//=5
+    return 1 if b==1 else 2
+
+#진짜 개허무하다..
