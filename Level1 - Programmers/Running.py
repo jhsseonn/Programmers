ba97507b2b28129445a2b50a4bc06789284f1123
jhsseonn <hs125bt@gmail.com>
2023-08-02 23:45:12 +0900

    230802 달리기 경주

--- Level1 - Programmers/Running.py
+++ Level1 - Programmers/Running.py
@@ -1,3 +1,10 @@
 def solution(players, callings):
-    answer = []
-    return answer
+    rate = {player: i for i, player in enumerate(players)}
+
+    for c in callings:
+        idx = rate[c]
+        rate[c] -= 1
+        rate[players[idx-1]] += 1
+        players[idx-1], players[idx] = players[idx], players[idx-1]
+
+    return players
