a52672d7b0583b13d4b4e907da0291e13dfe8d9a
jhsseonn <hs125bt@gmail.com>
2023-08-04 23:39:10 +0900

    230804 구명보트 테스트 실패

---
+++ Level2 - Programmers/LifeBoat.py
@@ -0,0 +1,22 @@
+# 테스트 죄다 실패^^
+
+# def solution(people, limit):
+#     answer = 0
+
+#     people = sorted(people)
+#     kg = 0
+
+#     for p in people:
+#         kg+=p
+#         if (kg>limit):
+#             answer+=1
+#             kg = p
+#         if people.index(p)==(len(people)-1):
+#             answer+=1
+
+#     return answer
+
+def solution(people, limit):
+    
+
+    return answer
