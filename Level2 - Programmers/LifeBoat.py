595e3e2213b9cef0b0c8c15f067508d1bd673ae6
jhsseonn <hs125bt@gmail.com>
2023-08-07 23:25:46 +0900

    230807 구명보트

--- Level2 - Programmers/LifeBoat.py
+++ Level2 - Programmers/LifeBoat.py
@@ -17,6 +17,16 @@
 #     return answer
 
 def solution(people, limit):
-    
+    answer = 0
 
-    return answer
+    people.sort()
+
+    a = 0
+    b = len(people)-1
+    while a<b:
+        if people[a]+people[b]<=limit:
+            a+=1
+            answer+=1
+        b-=1
+
+    return len(people)-answer
