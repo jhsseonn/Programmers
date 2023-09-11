5911ad78f259f4d4233a799a3cffcc3314d9e815
jhsseonn <hs125bt@gmail.com>
2023-09-11 23:58:28 +0900

    230911 programmers 구명보트

--- Level2 - Programmers/LifeBoat.py
+++ Level2 - Programmers/LifeBoat.py
@@ -16,6 +16,29 @@
 
 #     return answer
 
+# 그리디 알고리즘 공부 후 2번째 시도~
+
+def solution(people, limit):
+    answer = 0
+
+    people.sort()
+
+    start = 0
+    end = len(people)-1
+
+    while start < end:
+        if people[start]+people[end]<=limit:
+            answer+=1
+            start+=1
+            end-=1
+        else:
+            start+=1
+
+    return answer
+
+# 다른 사람 풀이...
+# 내가 생각한 방식대로라면 sort를 reverse로 해야했음.. 
+# 근데 오름차순으로 sort 해놓고 이게 왜 0이 나오지 하는 멍청한 생각을 하고 있었음 바본가
 def solution(people, limit):
     answer = 0
 
