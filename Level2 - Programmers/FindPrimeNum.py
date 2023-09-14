dad623c6eae8207c71b34823ed416422979c836b
jhsseonn <hs125bt@gmail.com>
2023-09-14 23:21:56 +0900

    230914 소수찾기 함수 구현

---
+++ Level2 - Programmers/FindPrimeNum.py
@@ -0,0 +1,16 @@
+import itertools
+
+def solution(numbers):
+    answer = 0
+    
+    
+    
+    return answer
+
+def isPrimeNum(num):
+    i=0
+    while i<num//2+1:
+        if num%i==0:
+            return False
+        i+=1
+    return True
