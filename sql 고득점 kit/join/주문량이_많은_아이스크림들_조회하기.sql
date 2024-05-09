35315ec91fabf7d52cddf1266bf0d12f81dc34de
jhsseonn <hs125bt@gmail.com>
2024-05-09 23:32:46 +0900

    240509 sql 고득점 kit join 문제풀이

---
+++ sql 고득점 kit/join/주문량이_많은_아이스크림들_조회하기.sql
@@ -0,0 +1,8 @@
+select flavor
+from (select flavor, total_order
+      from first_half
+      union all
+      select flavor, total_order
+      from july) as total_table
+group by flavor
+order by sum(total_order) desc limit 3
